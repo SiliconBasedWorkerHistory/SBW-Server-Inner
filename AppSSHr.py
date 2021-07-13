8a8a5415d121514bb3fec5c8b11ff7a3821a0398
alone-wolf <1031629044@qq.com>
2021-07-13 13:41:20 +0800

    update

--- AppSSHr.py
+++ AppSSHr.py
@@ -1,10 +1,45 @@
 import configparser
 import os
+import sys
 
 import paramiko
 from flask import Blueprint, Flask
 
 app = Blueprint("app_sshr_blurprint", __name__, url_prefix="/sshr")
+
+
+class Session:
+    host = None
+    port = None
+    username = None
+    password = None
+    cmd = None
+
+    def __init__(self, host, port, username, password, cmd):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.cmd = cmd
+
+
+class SessionConfig:
+    def __init__(self):
+        config_sample_out()
+        self.conf = configparser.ConfigParser()
+        self.conf.read("sshr.ini")
+
+    def list_session_name(self):
+        return self.conf.sections()
+
+    def get_session_by_name(self, name):
+        return Session(
+            self.conf.get(name, "host"),
+            self.conf.getint(name, "port"),
+            self.conf.get(name, "username"),
+            self.conf.get(name, "password"),
+            self.conf.get(name, "cmd")
+        )
 
 
 def config_sample_out():
@@ -22,12 +57,28 @@ def config_sample_out():
         conf.write(fw)
 
 
+def ssh_command_onetime1(session: Session):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=session.host,
+                port=int(session.port),
+                username=session.username,
+                password=session.password
+                )
+    stdin, stdout, stderr = ssh.exec_command(session.cmd)
+    print("err:", stderr.read().decode())
+    print("out:", stdout.read().decode())
+    print('执行完毕')
+    ssh.close()
+
+
 def ssh_command_onetime(host, port, username, password, cmd):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=host, port=int(port), username=username, password=password)
     stdin, stdout, stderr = ssh.exec_command(cmd)
-    print(stdout.read().decode())
+    print("err:", stderr.read().decode())
+    print("out:", stdout.read().decode())
     print('执行完毕')
     ssh.close()
 
@@ -64,3 +115,35 @@ def session_name(session_name):
 def init_app_sshr(server: Flask):
     config_sample_out()
     server.register_blueprint(app)
+
+
+def print_help(status):
+    print("help:")
+    print("AppSSHr.py <args>")
+    print("session: s session used to perform session by names")
+    print("list: l list used to list sessions")
+    exit(status)
+
+
+if __name__ == '__main__':
+
+    session_config = SessionConfig()
+
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print_help(-1)
+    if args[0] in ["-s", "--session"]:
+        if len(args) >= 2:
+            for i in args[1:]:
+                tmp_session_names = session_config.list_session_name()
+                if i not in tmp_session_names:
+                    print(f"err: find none of session name {i}, skipped")
+                else:
+                    print(f"find session of {i}, performing...")
+                    ssh_command_onetime1(session_config.get_session_by_name(i))
+        else:
+            print("err: please input session name")
+    elif args[0] in ["-l", "--list"]:
+        tmp = session_config.list_session_name()
+        for i in range(len(tmp)):
+            print(i + 1, tmp[i])
--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-import configparser
-import os
-
 from flask import Flask
+from configparser import ConfigParser
+import os
+import sys
 
 from AppSSHr import init_app_sshr
 
 app = Flask(__name__)
 
 
+class ServerConfig:
+    host = "0.0.0.0"
+    port = 5000
+    debug = True
+
+
 class Config:
+    config_file_name = "config.ini"
+    config_section_name = "Global"
+    server_config = None
+
     def __init__(self):
-        if os.path.exists("server_config.ini"):
-            return
-        conf = configparser.ConfigParser()
-        conf.add_section("server")
-        conf.set("server", "name", "inner-server")
-        conf.set("server", "host", "0.0.0.0")
-        conf.set("server", "port", "22")
-        conf.set("server", "debug", "True")
-        with open('server_config.ini', 'w') as fw:
-            conf.write(fw)
-
-
-def config_sample_out():
-    # ensure config file exists
-    if os.path.exists("server_config.ini"):
-        return
-    conf = configparser.ConfigParser()
-    conf.add_section("server")
-    conf.set("server", "name", "inner-server")
-    conf.set("server", "host", "0.0.0.0")
-    conf.set("server", "port", "22")
-    conf.set("server", "debug", "True")
-    with open('server_config.ini', 'w') as fw:
-        conf.write(fw)
+        self.server_config = ServerConfig()
+        self.conf = ConfigParser()
+        if not os.path.exists(self.config_file_name):
+            self.conf.add_section(self.config_section_name)
+            self.conf.set(self.config_section_name, "host", self.server_config.host)
+            self.conf.set(self.config_section_name, "port", str(self.server_config.port))
+            self.conf.set(self.config_section_name, "debug", str(self.server_config.debug))
+            with open(self.config_file_name, 'w') as fw:
+                self.conf.write(fw)
+        else:
+            self.conf.read(self.config_file_name)
+            self.server_config.host = self.conf.get(self.config_section_name, "host")
+            self.server_config.port = self.conf.getint(self.config_section_name, "port")
+            self.server_config.debug = self.conf.getboolean(self.config_section_name, "debug")
 
 
 init_app_sshr(app)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    c = Config()
+    app.run(host=c.server_config.host, port=c.server_config.port, debug=c.server_config.debug)
